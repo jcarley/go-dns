d1768274392673b344dba97197b53abfd33b378c
jcarley <jeff.carley@gettyimages.com>
2018-01-18 08:08:59 -0600

    WIP:  working through the process

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/dnsimple/dnsimple-go/dnsimple"
 )
@@ -20,15 +21,15 @@ const (
 
 func main() {
 
-	// DNSIMPLE_OAUTH_TOKEN
-
-	// 1) Load config
-	config, err := loadConfig(settingsFileName)
+	// First checks for DNSIMPLE_OAUTH_TOKEN environment variable.  If the
+	// variable is not found, checks the config file.
+	authToken, err := GetAuthToken()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading configuration: \n\n%s\n", err)
+		// need better logging
+		panic(err)
 	}
 
-	// 2) Look up the router's IP
+	// Look up the router's IP
 	routerIP, err := getRouterIP()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error determining router IP: \n\n%s\n", err)
@@ -36,32 +37,64 @@ func main() {
 
 	fmt.Printf("Router IP: %s\n", routerIP)
 
-	// 3) Instantiate a dnsimple client
-	client := dnsimple.NewClient(config.Token(), config.Email())
+	// new client
+	client := dnsimple.NewClient(dnsimple.NewOauthTokenCredentials(authToken))
+
+	// get the current authenticated account (if you don't know who you are)
+	whoamiResponse, err := client.Identity.Whoami()
+	if err != nil {
+		fmt.Printf("Whoami() returned error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// either assign the account ID or fetch it from the response
+	// if you are authenticated with an account token
+	fmt.Printf("%#v\n", whoamiResponse.Data.Account.ID)
+	accountID := strconv.Itoa(whoamiResponse.Data.Account.ID)
+
+	// get the list of domains
+	domainsResponse, err := client.Domains.ListDomains(accountID, &dnsimple.DomainListOptions{NameLike: "domain name here"})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// for _, domain := range domainsResponse.Data {
+	//   fmt.Printf("%#v\n", domain)
+	// }
+
+	domain := domainsResponse.Data[0]
+
+	zoneRecordsResponse, _ := client.Zones.ListRecords(accountID, domain.Name, &dnsimple.ZoneRecordListOptions{Type: "A"})
+	// zoneRecordsResponse, _ := client.Zones.ListRecords(accountID, domain.Name, nil)
+
+	fmt.Printf("%#v\n", zoneRecordsResponse.Data)
+
+	os.Exit(0)
 
 	// 4) Check the current IP of each domain
-	domains := config.LoadAllDomains()
-	for _, domain := range domains {
-		records, _ := client.Records(domain.Name, "", domain.RecordType)
-		for _, record := range records {
-			if record.RecordType == domain.RecordType {
-				if routerIP == record.Content {
-					fmt.Printf("DNS for '%s' matches\n", domain.Name)
-				} else {
-					// 5) If IP's don't match; update DNS record
-					fmt.Printf("DNS for '%s' does not match\n", domain.Name)
-					fmt.Printf("  Updating '%s' to '%s'\n", record.Content, routerIP)
-					record.UpdateIP(client, routerIP)
-				}
-			}
-		}
-	}
+	// domains := config.LoadAllDomains()
+	// for _, domain := range domains {
+	//   records, _ := client.Records(domain.Name, "", domain.RecordType)
+	//   for _, record := range records {
+	//     if record.RecordType == domain.RecordType {
+	//       if routerIP == record.Content {
+	//         fmt.Printf("DNS for '%s' matches\n", domain.Name)
+	//       } else {
+	//         // 5) If IP's don't match; update DNS record
+	//         fmt.Printf("DNS for '%s' does not match\n", domain.Name)
+	//         fmt.Printf("  Updating '%s' to '%s'\n", record.Content, routerIP)
+	//         record.UpdateIP(client, routerIP)
+	//       }
+	//     }
+	//   }
+	// }
 
 }
 
 func getRouterIP() (string, error) {
 
-	url := "http://jsonip.com"
+	url := "https://jsonip.com"
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Add("Accept", "application/json")
